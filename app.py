f36330c3655a93476de38c0f23910a7cd2e95f8e
jhorsax16 <jhorsax161616@gmail.com>
2022-08-03 12:22:08 -0500

    Añadiendo permisos para que localhost:8000 pueda realizar peticiones a nuestra API (CORSMiddleware)

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI
 from routes.asistente import asistente
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="API para Gestion de Medicamentos y Equipos",
     description="Esta API está echa para la gestion de medicamentos y equipos médicos de un hospital grande como el de Essalud",
+)
+
+origins = [
+    "http://localhost:8000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
 )
 
 app.include_router(asistente)
